7801ac17029e7584058fb9bae5e2cc0f151d8385
Dagistan Karadeniz <dagistankaradeniz@gmail.com>
2019-12-27 21:33:40 +0000

    cloudformation stack delete / cleanup command added

--- commands.sh
+++ commands.sh
@@ -9,3 +9,8 @@ aws cloudformation package --s3-bucket dagistan-sam --template-file template.yam
 # deploy
 aws cloudformation deploy --template-file gen/template-generated.yaml --stack-name hello-world-sam --capabilities CAPABILITY_IAM
 # sam deploy...    instead of    aws cloudformation deploy     can be used
+
+
+
+# DELETE CLOUDFORMATION STACK. run command below to delete stack and related resources
+# aws cloudformation delete-stack --stack-name hello-world-sam
