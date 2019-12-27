b023d144c1a2effe6aae979bf629c9cad434cadb
Dagistan Karadeniz <dagistankaradeniz@gmail.com>
2019-12-27 21:21:38 +0000

    aws lambda api gateway dynamodb sam

--- src/app.py
+++ src/app.py
@@ -4,6 +4,11 @@ import os
 
 print('Loading function')
 
+# always create client outside the handler
+region_name = os.environ['REGION_NAME']
+dynamo = boto3.client('dynamodb', region_name=region_name)
+table_name = os.environ['TABLE_NAME']
+
 
 def respond(err, res=None):
     return {
@@ -16,7 +21,6 @@ def respond(err, res=None):
 
 
 def lambda_handler(event, context):
-
     print("Received event: " + json.dumps(event, indent=2))
-
-    return respond(None, res="Hello World")
+    scan_result = dynamo.scan(TableName=table_name)
+    return respond(None, res=scan_result)
